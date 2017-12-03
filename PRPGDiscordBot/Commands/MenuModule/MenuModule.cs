fec42aa47d62a9c0b0a39fa2cf4cf0cccf94b61c
SoaringDylan <dylanpiera@Hotmail.nl>
2017-12-03 18:16:32 +0100

    GenerateOptions
    Start of this method, realised I'm gonna rework this, see the issue I'll open in a moment :)

--- PRPGDiscordBot/Commands/MenuModule/MenuModule.cs
+++ PRPGDiscordBot/Commands/MenuModule/MenuModule.cs
@@ -7,10 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PRPGDiscordBot.Commands.MenuModule
+namespace PRPGDiscordBot.Commands.Menu
 {
     public class MenuModule : ModuleBase
     {
+        public struct MenuStruct
+        {
+            public EmbedBuilder Builder;
+            public giveOptions Next;
+
+            public MenuStruct(EmbedBuilder builder = null, giveOptions next = null)
+            {
+                Builder = builder;
+                Next = next;
+            }
+        }
+
         public static Dictionary<ulong, Func<SocketMessage, Task>> Events = new Dictionary<ulong, Func<SocketMessage, Task>>();
 
         public delegate Task giveOptions(SocketMessage socket, DiscordSocketClient client);
@@ -25,18 +37,34 @@ namespace PRPGDiscordBot.Commands.MenuModule
                 return;
             }
 
-            giveOptions options = OptionGenerator(i => i == "test", u => u == Context.User.Id);   
+            giveOptions options = OptionGenerator(new Dictionary<Func<string, bool>, MenuStruct>()
+            {
+                {ContentValidizer("yes") , new MenuStruct(new EmbedBuilder() {Description = "You answered yes."},
+                    OptionGenerator(ContentValidizer("no"), IsSameUserAs(Context.User.Id), new EmbedBuilder() {Description = "You had a change of mind, I see?" }))},
+                {ContentValidizer("no") , new MenuStruct(new EmbedBuilder() {Description = "You answered no." }, 
+                OptionGenerator(ContentValidizer("yes"), IsSameUserAs(Context.User.Id), new EmbedBuilder() {Description = "You had a change of mind, I see?" }))}
+            }, IsSameUserAs(Context.User.Id), false);   
 
             Func<SocketMessage, Task> eventHandler = async (s) => await options(s, Context.Client as DiscordSocketClient);
             Events.Add(Context.User.Id, eventHandler);
             (Context.Client as DiscordSocketClient).MessageReceived += Events[Context.User.Id];
         }
 
+        public static Func<string, bool> ContentValidizer(string validizerString)
+        {
+            return (s) => s == validizerString;
+        }
+
+        public static Func<ulong, bool> IsSameUserAs(ulong user)
+        {
+            return (u) => u == user;
+        }
+
         public static giveOptions OptionGenerator(Func<string, bool> contentValidizer, Func<ulong, bool> userValidizer, EmbedBuilder builder = null,  bool endOfChain = true, giveOptions next = null)
         {
             return async (s, c) =>
             {
-                if (userValidizer(s.Author.Id))
+                if (!userValidizer(s.Author.Id))
                     return;
                 if (contentValidizer(s.Content))
                 {
@@ -54,6 +82,41 @@ namespace PRPGDiscordBot.Commands.MenuModule
                         c.MessageReceived -= Events[s.Author.Id];
                         Events[s.Author.Id] = async (sm) => await next(sm, c);
                         c.MessageReceived += Events[s.Author.Id];
+                    }
+                }
+            };
+        }
+
+        public static giveOptions OptionGenerator(Dictionary<Func<string,bool>, MenuStruct> dictionary, Func<ulong, bool> userValidizer, bool endOfChain = true)
+        {
+            return async (s, c) =>
+            {
+                if (!userValidizer(s.Author.Id))
+                    return;
+
+                foreach (Func<string,bool> item in dictionary.Keys)
+                {
+                    if(item(s.Content))
+                    {
+                        EmbedBuilder builder = dictionary[item].Builder;
+                        giveOptions next = dictionary[item].Next;
+
+                        if(builder == null)
+                            builder = new EmbedBuilder() { Description = "Error. No message found." };
+                        await s.Channel.SendMessageAsync("", false, builder.Build());
+
+                        if (endOfChain)
+                        {
+                            c.MessageReceived -= Events[s.Author.Id];
+                            Events.Remove(s.Author.Id);
+                        }
+                        else
+                        {
+                            c.MessageReceived -= Events[s.Author.Id];
+                            Events[s.Author.Id] = async (sm) => await next(sm, c);
+                            c.MessageReceived += Events[s.Author.Id];
+                        }
+                        return;
                     }
                 }
             };
--- PRPGDiscordBot/Commands/RegistrationCommand.cs
+++ PRPGDiscordBot/Commands/RegistrationCommand.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using PRPGDiscordBot.Commands.Menu;
 
 namespace PRPGDiscordBot.Commands
 {
